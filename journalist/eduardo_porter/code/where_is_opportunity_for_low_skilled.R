02f778472fab71f8bade30628c86c8933fe46770
anisfeld <ari.anisfeld@gmail.com>
2019-03-15 12:37:30 -0500

    #9 getting agreement between output

--- journalist/eduardo_porter/code/where_is_opportunity_for_low_skilled.R
+++ journalist/eduardo_porter/code/where_is_opportunity_for_low_skilled.R
@@ -1,5 +1,10 @@
+# Ari Version -- Depricated
+
 # Where should and where do low-skill (high-skill) workers move today?
 # convergence issue #3 and #6
+
+# This code relies on data produced where_is_opportunity_for_low_skills_core_results.R and
+# is NOT MEANT FOR POSTERITY.
 
 rm(list=ls())
 library(tidyverse)
@@ -28,44 +33,7 @@ out_path <- make_path("journalist/eduardo_porter/out")
 # Data loading functions #
 ##########################
 
-load_ipums_sample <- function(level, 
-                              fresh_run = TRUE,
-                              raw_ipums_in_memory = FALSE,
-                              src_path. = src_path,
-                              dropbox_path. = dropbox_path)
-  {
-  
-  stopifnot(level %in% c("high_skill", "low_skill"))
-  
-  if (fresh_run) {
-    if (!raw_ipums_in_memory){
-      raw_ipums <- read_dta(file.path(dropbox_path., "full_ipums_extract.dta")) %>% 
-        select(-c(empstatd, related, educ, perwt))
-      }
-     
-      ipums_sample <-
-          raw_ipums %>%
-            group_by(serial) %>%
-            mutate(incwage = ifelse(incwage >= 999998, NA, incwage),
-                   hhinc = sum(incwage, na.rm = TRUE),
-                   annual_housing = 12*ifelse(owncost == 99999, rentgrs, owncost),
-                   real_wage = hhinc - annual_housing) %>%
-            filter(age >= 25, age <= 65, relate == 1, empstat %in% c(1,2)) %>%
-            filter(if(level == "low-skill") educd < 101 else  educd >= 101)
-    
-      ipums_sample %>%  write_rds(file.path(src_path., glue::glue("{level}_filtered_ipums_with_hhinc.rds"))) 
-      
-    } else {
-      ipums_sample <- read_rds(file.path(src_path., glue::glue("{level}_filtered_ipums_with_hhinc.rds"))) 
-    }
-  
-  ipums_sample
-  }
-    
-
-
-load_puma_to_migpuma <- function(puma_population_from_scratch, 
-                                 src_path. = src_path){
+load_puma_to_migpuma <- function(src_path. = src_path){
     state_fip_to_name <- read_csv(file.path(src_path., "state_fip_to_name.csv"))
     
     puma_to_migpuma_2010 <- read_xls(file.path(src_path., "puma_migpuma1_pwpuma00.xls"), skip=3, 
@@ -102,7 +70,7 @@ load_top_line_place <- function(puma_to_migpuma_2010,
       puma_data_2016 %>%
       left_join(puma_to_migpuma_2010, by=c("puma", "statefip")) %>%
       mutate(name = str_replace_all(name, "( \\(.*|--.*| PUMA)", ""),
-             migpuma1 = as.numeric(migpuma1)) %>%
+             migpuma1 = as.numeric(res_puma)) %>%
       group_by(statefip, migpuma1, name) %>%
       summarize(population_by_town = sum(population))  %>%
       # this step helps capture name of most populous puma
@@ -120,133 +88,102 @@ load_top_line_place <- function(puma_to_migpuma_2010,
     top_line_place_2016
 }
 
-
-
 ############################
 #  Data Munging Functions  #
 ############################
-get_msa_net_migration_df <- function(level, 
-                                    ipums_data,  
-                                     src_path. = src_path)
-  {
-  
-  msa_data <- read_xls(file.path(src_path.,"MSA2013_PUMA2010_crosswalk.xls"))  %>%
-              mutate_at(c("MSA Code", "PUMA Code", "State FIPS Code"), as.numeric) %>% 
-              select(starts_with("MSA")) %>%
-              dplyr::distinct(`MSA Title`, .keep_all=TRUE)
-  
-  msa_out_migration <-
-    ipums_sample %>%
-    ungroup()  %>%
-    # Only include households that moved
-    filter(migpuma1 > 0) %>%
-    group_by(migmet131) %>%
-      summarize(n_out = sum(hhwt, na.rm=TRUE)) %>%
-    transmute(migmet131, n_out)
-  
-  msa_main_output <-
-    ipums_sample %>%
-    group_by(met2013) %>%
-    summarize(
-      n_obs_for_median_real_wage = n(),
-      mean_nominal_wage = weighted.mean(hhinc, w=hhwt, na.rm=TRUE),
-      median_nominal_wage = matrixStats::weightedMedian(hhinc, w=hhwt, na.rm = TRUE),
-      mean_real_wage = weighted.mean(real_wage, w=hhwt, na.rm=TRUE),
-      median_real_wage = matrixStats::weightedMedian(real_wage, w=hhwt, na.rm = TRUE),
-      n_in = sum(ifelse(migpuma1 > 0, hhwt,0))) %>%
-    full_join(msa_out_migration, by=c("met2013" = "migmet131")) %>%
-    left_join(msa_data,
-              by =c("met2013" = "MSA Code")) %>%
-    mutate(n_out = ifelse(is.na(n_out), 0, n_out),
-             `raw net migration`= n_in - n_out,
-             `net migration` = `raw net migration`/`MSA 2010 Population`) %>%
-    select(`MSA Title`, `net migration`,`raw net migration`, everything()) %>%
-    arrange(desc(`net migration`)) 
-  
-}  
+migpuma_real_wages_ <-
+  ipums_sample %>% 
+  select(incwage, real_wage, statefip, puma, skill, hhwt) %>%
+  left_join(puma_to_migpuma_2010, by = c("statefip", "puma")) %>% 
+  rename(migpuma1 = res_puma, migplac1 = res_state) %>%
+  group_by(migpuma1, migplac1, skill) %>%
+  summarize(
+    state = first(res_stname), 
+    mean_nominal_wage = weighted.mean(incwage, w=hhwt, na.rm=TRUE),
+    mean_real_wage = weighted.mean(real_wage, w=hhwt, na.rm=TRUE)
+  )
 
-get_migpuma_net_migration_df <- function(level, 
-                                    ipums_sample)
-  {
-    
-    migpuma_in_migration <-
+
+  migpuma_in_migration_ <-
       ipums_sample %>%
-        ungroup()  %>%
-        # Only include households that moved
-        filter(migplac1 > 0) %>% 
-        group_by(statefip, puma) %>%
-        summarize(n_obs_n_in = n(), 
-                  n_in = sum(hhwt, na.rm=TRUE),
-                  n_in_not_foreign = sum(ifelse(migplac1  < 100, hhwt, 0))) %>%
-        ungroup() %>%
+        filter(migpuma1 > 2) %>% 
+        # filter(migplac1 > 0) %>% 
+        group_by(statefip, puma, skill) %>%
+        summarize(n_in = sum(hhwt, na.rm=TRUE)) %>%
         left_join(puma_to_migpuma_2010, by = c("puma", "statefip")) %>%
-        group_by(migpuma1, migplac1) %>%
-        summarise(n_in = sum(n_in),
-                  n_obs_n_in = sum(n_obs_n_in),
-                  n_in_not_foreign = sum(n_in_not_foreign))
+      rename(migpuma1 = res_puma, migplac1 = res_state) %>%
+        group_by(migpuma1, migplac1, skill) %>%
+        summarise(n_in = sum(n_in))
     
-    migpuma_out_migration <-
+    migpuma_out_migration_ <-
       ipums_sample %>%
-        ungroup()  %>%
-        # Only include households that moved
-        filter(migplac1 > 0) %>% 
-        group_by(migpuma1, migplac1) %>%
+        filter(migpuma1 > 2) %>% 
+        # filter(migplac1 > 0) %>% 
+        group_by(migpuma1, migplac1, skill) %>%
         summarize(n_out = sum(hhwt, na.rm=TRUE),
                   n_out_not_foreign = sum(ifelse(migplac1 < 100, hhwt, 0))) %>%
         ungroup() %>%
-        transmute(migpuma1, migplac1, n_out, n_out_not_foreign)
+        transmute(migpuma1, migplac1, skill,  n_out, n_out_not_foreign)
     
-    migpuma_real_wages <-
-      ipums_sample %>% 
-        ungroup() %>%
-        select(hhinc, real_wage, statefip, puma, hhwt) %>%
-        left_join(puma_to_migpuma_2010, by = c("statefip", "puma")) %>%   
-        group_by(migpuma1, migplac1) %>%
-        summarize(
-          state = first(stname), 
-          n_obs_for_median_real_wage = n(),
-          mean_nominal_wage = weighted.mean(hhinc, w=hhwt, na.rm=TRUE),
-          median_nominal_wage = matrixStats::weightedMedian(hhinc, w=hhwt, na.rm = TRUE),
-          median_real_wage = matrixStats::weightedMedian(real_wage, w=hhwt, na.rm = TRUE),
-          mean_real_wage = weighted.mean(real_wage, w=hhwt, na.rm=TRUE)
-        )
+    place_pop_ <-
+      place_pop %>%
+      rename(migpuma1 = res_puma, migplac1 = res_state)
     
-    migpuma_main_output <-
-      migpuma_real_wages %>%
+    place_wages_nominal_all_ <- 
+      # ipums_sample %>% 
+      # filter(migpuma1 <= 2) %>% 
+      # group_by(res_state, res_puma) %>%
+      # summarise(nominal_wage_everyone = weighted.mean(hh_wage_inc, hhwt)) %>%
+      place_wages_nominal_all %>%
+      rename(migpuma1 = res_puma, migplac1 = res_state)
+    
+
+    
+    migpuma_main_output_ <-
+      migpuma_real_wages_ %>%
       left_join(top_line_place_2016, by=c("migpuma1", "migplac1" = "statefip")) %>%
-      full_join(migpuma_out_migration, by=c("migpuma1", "migplac1")) %>%
-      full_join(migpuma_in_migration, by=c("migpuma1", "migplac1")) %>%
-      mutate(n_out = ifelse(is.na(n_out), 0, n_out),
+      full_join(migpuma_out_migration_, by=c("migpuma1", "migplac1", "skill" = "skill")) %>%
+      full_join(migpuma_in_migration_, by=c("migpuma1", "migplac1",  "skill" = "skill")) %>% 
+      full_join(place_pop_, by=c("migpuma1", "migplac1")) %>% 
+      full_join(place_wages_nominal_all_, by=c("migpuma1", "migplac1")) %>% 
+        mutate(n_out = ifelse(is.na(n_out), 0, n_out),
              n_in = ifelse(is.na(n_in), 0, n_in),
              `raw net migration` = n_in - n_out, 
-             `net migration`= `raw net migration`/migpuma_population,
-             `net migration (not foreign)` = (n_in_not_foreign - n_out_not_foreign)/migpuma_population) %>%
-      mutate(migpuma_name = ifelse(is.na(migpuma_name), str_c(migplac1, "_", migpuma1), migpuma_name)) %>%
-      ungroup() %>%
-      select(state, migpuma_name, `net migration`, `raw net migration`, everything(), -c(migplac1, n_in_not_foreign, n_out_not_foreign)) %>%
-      arrange(desc(`net migration`)) 
+             net_mig = 1000*`raw net migration`/pop,
+             net_mig_full = 1000*`raw net migration`/migpuma_population) %>%
+      select(skill, state, migpuma_name, net_mig, `raw net migration`, everything())
     
-  }
-
+  
 ############
 # RUN CODE #
 ############
 puma_to_migpuma_2010 <- load_puma_to_migpuma()
 top_line_place_2016 <- load_top_line_place(puma_to_migpuma_2010)
 
-fresh_run = FALSE
-raw_ipums_in_memory = TRUE
+    
+main_filter_on_plac1 <- main_filter_on_plac1  %>% 
+      full_join(place_wages_nominal_all_, by=c("migpuma1", "migplac1"))
 
-for(level in c("high_skill", "low_skill")){
-   ipums_sample <- load_ipums_sample(level, fresh_run, raw_ipums_in_memory)
+skill_filter = c("low_skill", "high_skill", "low_skill", "high_skill")
+wage_type = c(rep("nominal_wage_everyone", 2), "mean_real_wage", "mean_real_wage")
+mig_type = c(rep("net_mig", 4))
+x_label = c(rep("Nominal income ($000s)", 2),
+            "Income - housing cost for low skill ($000s)",
+            "Income - housing cost for high skill ($000s)")
 
-   msa_main_output <- get_msa_net_migration_df(level, ipums_sample)
-   
-   msa_main_output %>%
-     write_csv(file.path(out_path, glue::glue("{level}_migration_by_msa.csv")))
+convergence_plots <- pmap(list(wage_type=wage_type, mig_type=mig_type, 
+                               x_label=x_label, skill_filter=skill_filter),
+                              make_plot, 
+                              data=migpuma_main_output_,
+                              weights="pop")
 
-   migpuma_main_output <-get_migpuma_net_migration_df(level, ipums_sample)
-   
-   migpuma_main_output %>%
-     write_csv(file.path(out_path, glue::glue("{level}_migration_by_migpuma.csv")))
-}
+(grid_of_plots <- 
+    grid.arrange(
+      convergence_plots[[1]], 
+      convergence_plots[[2]],
+      convergence_plots[[3]],
+      convergence_plots[[4]])
+)
+
+
+ggsave("temp_for_peter.png", grid_of_plots, width=7, height = 5)
